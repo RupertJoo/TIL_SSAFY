ebda990c4bca3acd346183c7377821538ab2e770
rupertjoo <rokaf0743@gmail.com>
2023-07-21 17:56:25 +0900

    c1756

---
+++ solve1859.py
@@ -0,0 +1,13 @@
+def solve1859():
+    for t in range(1, int(input()) + 1):
+        n = int(input())
+        values = list(map(int, input()))
+        
+        benefit = 0
+        for i in values:
+            pass
+        ans = 0
+        print(f"{t} {ans}")
+
+if __name__ == "__main__":
+    solve1859()
---
+++ solve2072.py
@@ -0,0 +1,11 @@
+# 2072. 홀수만 더하기
+
+def solve2072():
+    for t in range(1, int(input()) + 1):
+        list_t = list(map(int, input().split()))
+        list_ans = [i for i in list_t if i % 2 == 1]
+        ans = sum(list_ans)
+        print(f"#{t} {ans}")
+
+if __name__ == "__main__":
+    solve2072()
